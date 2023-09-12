8ac41a48f95d469b59b3fe97956973212d52605e
Joseph Hoang <117396414+jhoang949@users.noreply.github.com>
2023-09-12 13:50:43 -0700

    Card Class
    -Card class and its implementation
    -Contains value, rank, and suit
    -methods include getters that retrieve private variables

---
+++ Cards.cpp
@@ -0,0 +1,73 @@
+//
+// Created by Joseph Hoang on 8/24/23.
+//
+#include "Cards.h"
+
+// Card Constructor - takes value and suit and assigns rank based on value
+Card::Card(unsigned val, char sut){
+    value = val;
+    suit = sut;
+    // Assigns rank based on value of the card
+    switch (value){
+        case 1:
+            rank = "A";
+            break;
+        case 2:
+            rank = "2";
+            break;
+        case 3:
+            rank = "3";
+            break;
+        case 4:
+            rank = "4";
+            break;
+        case 5:
+            rank = "5";
+            break;
+        case 6:
+            rank = "6";
+            break;
+        case 7:
+            rank = "7";
+            break;
+        case 8:
+            rank = "8";
+            break;
+        case 9:
+            rank = "9";
+            break;
+        case 10:
+            rank = "10";
+            break;
+        case 11:
+            rank = "J";
+            break;
+        case 12:
+            rank = "Q";
+            break;
+        case 13:
+            rank = "K";
+            break;
+        case 14:
+            rank = "A";
+            break;
+    };
+}
+
+// Card getters
+
+// Gets card value
+unsigned Card::getValue() {
+    return value;
+}
+
+// Gets card rank
+string Card::getRank() {
+    return rank;
+}
+
+// Gets card suit
+char Card::getSuit() {
+    return suit;
+}
+
---
+++ Cards.h
@@ -0,0 +1,30 @@
+//
+// Created by Joseph Hoang on 8/24/23.
+//
+
+#ifndef POKER_CARDS_H
+#define POKER_CARDS_H
+
+#include "string"
+using namespace std;
+
+// Card Class
+class Card{
+private:
+    unsigned value=0;
+    string rank="0";
+    char suit='S';
+
+public:
+    // Card constructor - takes value and suit and assigns rank based on value
+    Card(unsigned val, char sut);
+
+    // Card getters for value, rank and suit
+    unsigned getValue();
+    string getRank();
+    char getSuit();
+};
+
+
+
+#endif //POKER_CARDS_H
